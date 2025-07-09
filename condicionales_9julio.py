a793f5aad5775dd8b1df59cc67ef65307e190b4a
Yessica Mayor <yessicamayor1@gmail.com>
2025-07-09 17:24:10 -0500

    taller, por ahora 10

---
+++ condicionales_9julio.py
@@ -0,0 +1,24 @@
+#2. calcula el mayor de dos numeros ingresados
+n1=int(input("ingrese el  primer num: "))
+n2=int(input("ingrese el segundo numero: "))
+if n1>n2:
+    print(f"el mayor es {n1}")
+elif n2>n1:
+    print(f"el mayor es el {n2}")
+
+
+#-------------------------------------------TALLER 10 PREGUNTAS-----------------------------------------------
+#PRIMERO
+n1=int(input("ingrese el  primer num: "))
+
+if n1>0:
+    print(f"el numero positivo es {n1}")
+elif n1<0:
+    print(f"el numero negativo es {n1}")
+
+else n1>=0:
+
+
+
+
+
